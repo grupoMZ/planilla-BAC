7fa8fdd8aba7e1f41f71840e1f6f086e58a391f7
P. Zometa <zometa@anixet.com>
2022-05-01 19:45:26 +0200

    Update main file to print used config. file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use anyhow::{Context, Result};
 use planilla_bac as pbac;
+use std::env;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Ejecutando archivo: \"{}\"", args[0]);
+
     pbac::print_version();
 
     let date = pbac::get_date();
     let envio = pbac::get_envio_correlative();
-    let r = pbac::gen_files(date, envio);
+    let config = pbac::get_config_file_name(args);
+    let r = pbac::gen_files(date, envio, config);
     match r {
         Ok(()) => pbac::display_success().context(""),
         Err(e) => pbac::display_error(e.to_string()).context("Cerrando aplicación"),
